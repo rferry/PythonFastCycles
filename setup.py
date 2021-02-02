468120255d179979864e391cfb7ac8e07eeb5536
Roxane Ferry <69057601+rferry@users.noreply.github.com>
2021-02-02 15:33:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rferry/PythonFastCycles",
-    packages=setuptools.find_packages(),
+    packages=['process_data', 'tools', 'simulation'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License (GPL)",
