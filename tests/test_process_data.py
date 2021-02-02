5c021a21c08e5297a83b3427675a9f226774dd70
Roxane Ferry <ferry.roxane@gmail.com>
2021-02-02 17:00:20 +0100

    Add tests files

---
+++ tests/test_process_data.py
@@ -0,0 +1,6 @@
+"""
+Tests for the class ReadData
+
+First version by R. Ferry on February 2021.
+"""
+from process_data import ReadData 
---
+++ tests/test_simulation.py
@@ -0,0 +1,10 @@
+"""
+Tests for the class Simulation
+
+First version by R. Ferry on February 2021.
+"""
+import numpy as np
+from simulation import Simulation
+
+def test_create_all_files(tmpdir):
+    pass 
