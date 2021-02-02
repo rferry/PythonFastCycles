1048510fcd735bb7ff2f5d938db2f4191624deb5
Roxane Ferry <ferry.roxane@gmail.com>
2021-02-02 16:43:31 +0100

    new version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PythonFastCycles", 
-    version="1.0.1",
+    version="1.0.2",
     author="Roxane Ferry",
     author_email='roxane.ferry@ens.fr',
     description="Package to interact with FastCycles",
